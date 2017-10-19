d172c4658440d0ff0233bb9934824d65036ad396
Edward Wilde <ewilde@gmail.com>
2017-10-19 21:54:49 +0100

    Fix build

--- examples/test.tf
+++ examples/test.tf
@@ -12,7 +12,7 @@ provider "form3" {
 
 resource "form3_user" "admin_user" {
   organisation_id = "${var.organisation_id}"
-  user_id = "44247ebb-fe01-44ab-887d-7f344481712f"
+  user_id = "56f36c4b-8df8-4577-8c4b-8d32f32210f0"
   user_name = "terraform-user"
   email = "terraform-user@form3.tech"
   roles = ["${form3_role.role.role_id}"]
