f9c537ccec6d1ea5dcbf00c88f1c2b5e7ff5ea21
enriquerecarte <35294784+enriquerecarte@users.noreply.github.com>
2018-02-26 16:36:48 +0000

    Added bacs association (#6)
    * Added bacs association

---
+++ associations_test.go
@@ -0,0 +1,142 @@
+package form3
+
+import (
+	"fmt"
+	"github.com/ewilde/go-form3/client/associations"
+	"github.com/ewilde/go-form3/models"
+	"github.com/go-openapi/strfmt"
+	"github.com/nu7hatch/gouuid"
+	"testing"
+)
+
+func TestDeleteBacsAssociation(t *testing.T) {
+
+	serviceUserNumber := "123456"
+	id, _ := uuid.NewV4()
+	createResponse, err := auth.AssociationClient.Associations.PostBacs(associations.NewPostBacsParams().
+		WithCreationRequest(&models.BacsAssociationCreation{
+			Data: &models.BacsNewAssociation{
+				ID:             strfmt.UUID(id.String()),
+				OrganisationID: strfmt.UUID(organisationId.String()),
+				Attributes: &models.BacsAssociationAttributes{
+					ServiceUserNumber: serviceUserNumber,
+				},
+			},
+		}))
+
+	assertNoErrorOccurred(err, t)
+	_, err = auth.AssociationClient.Associations.DeleteBacsID(associations.NewDeleteBacsIDParams().
+		WithID(createResponse.Payload.Data.ID),
+	)
+
+	assertNoErrorOccurred(err, t)
+
+	_, err = auth.AssociationClient.Associations.GetBacsID(associations.NewGetBacsIDParams().
+		WithID(createResponse.Payload.Data.ID))
+
+	assertStatusCode(err, t, 404)
+}
+
+func TestGetBacsForNonExistingAssociation(t *testing.T) {
+	randomId, _ := uuid.NewV4()
+	randomUUID := strfmt.UUID(randomId.String())
+
+	_, err := auth.AssociationClient.Associations.GetBacsID(associations.NewGetBacsIDParams().
+		WithID(randomUUID))
+
+	assertStatusCode(err, t, 404)
+}
+
+func TestGetBacsAssociation(t *testing.T) {
+	serviceUserNumber := "987892"
+	id, _ := uuid.NewV4()
+	createResponse, err := auth.AssociationClient.Associations.PostBacs(associations.NewPostBacsParams().
+		WithCreationRequest(&models.BacsAssociationCreation{
+			Data: &models.BacsNewAssociation{
+				ID:             strfmt.UUID(id.String()),
+				OrganisationID: strfmt.UUID(organisationId.String()),
+				Attributes: &models.BacsAssociationAttributes{
+					ServiceUserNumber: serviceUserNumber,
+				},
+			},
+		}))
+
+	assertNoErrorOccurred(err, t)
+
+	_, err = auth.AssociationClient.Associations.GetBacsID(associations.NewGetBacsIDParams().
+		WithID(createResponse.Payload.Data.ID),
+	)
+
+	assertNoErrorOccurred(err, t)
+
+	_, err = auth.AssociationClient.Associations.DeleteBacsID(associations.NewDeleteBacsIDParams().
+		WithID(createResponse.Payload.Data.ID),
+	)
+
+	assertNoErrorOccurred(err, t)
+}
+
+func TestPostBacsAssociation(t *testing.T) {
+	serviceUserNumber := "987897"
+	id, _ := uuid.NewV4()
+	createResponse, err := auth.AssociationClient.Associations.PostBacs(associations.NewPostBacsParams().
+		WithCreationRequest(&models.BacsAssociationCreation{
+			Data: &models.BacsNewAssociation{
+				ID:             strfmt.UUID(id.String()),
+				OrganisationID: strfmt.UUID(organisationId.String()),
+				Attributes: &models.BacsAssociationAttributes{
+					ServiceUserNumber: serviceUserNumber,
+				},
+			},
+		}))
+
+	assertNoErrorOccurred(err, t)
+	actualOrganisationId := createResponse.Payload.Data.OrganisationID.String()
+	actualServiceUserNumber := createResponse.Payload.Data.Attributes.ServiceUserNumber
+	if actualOrganisationId != organisationId.String() {
+		t.Fatalf("Expected %s, got %s", organisationId.String(), actualOrganisationId)
+	}
+
+	if actualServiceUserNumber != serviceUserNumber {
+		t.Fatalf("Expected %s, got %s", serviceUserNumber, actualServiceUserNumber)
+	}
+	_, err = auth.AssociationClient.Associations.DeleteBacsID(associations.NewDeleteBacsIDParams().
+		WithID(createResponse.Payload.Data.ID),
+	)
+
+	assertNoErrorOccurred(err, t)
+
+}
+
+func TestGetBacsAssociationList(t *testing.T) {
+	id, _ := uuid.NewV4()
+	organisationIdUUID := strfmt.UUID(organisationId.String())
+	serviceUserNumber := "123456"
+	createResponse, err := auth.AssociationClient.Associations.PostBacs(associations.NewPostBacsParams().
+		WithCreationRequest(&models.BacsAssociationCreation{
+			Data: &models.BacsNewAssociation{
+				ID:             strfmt.UUID(id.String()),
+				OrganisationID: strfmt.UUID(organisationId.String()),
+				Attributes: &models.BacsAssociationAttributes{
+					ServiceUserNumber: serviceUserNumber,
+				},
+			},
+		}))
+
+	assertNoErrorOccurred(err, t)
+
+	getBacsResponse, err := auth.AssociationClient.Associations.GetBacs(associations.NewGetBacsParams().
+		WithFilterOrganisationID(&organisationIdUUID))
+
+	//getBacsResponse, err := auth.AssociationClient.Associations.GetBacs(associations.NewGetBacsParams())
+
+	fmt.Println(getBacsResponse)
+	assertNoErrorOccurred(err, t)
+
+	_, err = auth.AssociationClient.Associations.DeleteBacsID(associations.NewDeleteBacsIDParams().
+		WithID(createResponse.Payload.Data.ID),
+	)
+
+	assertNoErrorOccurred(err, t)
+
+}
