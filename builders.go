3469882e26792487c8e9054ebfcf73f7dbeec6a3
Edward Wilde <ewilde@gmail.com>
2018-03-27 22:00:12 +0100

    bug: Fix length of auto generated schemetransaction id

--- builders.go
+++ builders.go
@@ -82,9 +82,9 @@ func (b *PaymentBuilder) WithSchemeTransactionID(schemeTransactionID string) *Pa
 }
 
 func (b *PaymentBuilder) NewSchemeTransactionID() string {
-	timestamp := time.Now().UTC().Format("yyyyMMddHHmmss")
-	uniqueId := math.Mod(rand.Float64(), 100000000000000000)
-	return timestamp + fmt.Sprintf("%f", uniqueId)
+	timestamp := time.Now().UTC().Format("20060102150405")
+	uniqueId := math.Mod(float64(rand.Int63n(math.MaxInt64)), 100000000000000000)
+	return timestamp + fmt.Sprintf("%17.0f", uniqueId)
 }
 
 func (b *PaymentBuilder) Build() *models.Payment {
--- builders_test.go
+++ builders_test.go
@@ -1,1 +1,11 @@
 package form3
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSchemeTransactionID(t *testing.T) {
+	builder := PaymentBuilder{}
+	assert.Equal(t, 31, len(builder.NewSchemeTransactionID()))
+}
