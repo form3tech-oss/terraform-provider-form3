c9077bf619be928767ea05f6ecfa0a0e1419325b
Kevin Holditch <kevholditch@hotmail.com>
2018-05-30 16:09:06 +0100

    Add sepa instant associations (#13)
    feat: sepa instant association

---
+++ form3/resource_sepainstant_association_test.go
@@ -0,0 +1,97 @@
+package form3
+
+import (
+	"fmt"
+	"github.com/ewilde/go-form3"
+	"github.com/ewilde/go-form3/client/associations"
+	"github.com/go-openapi/strfmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/satori/go.uuid"
+	"os"
+	"testing"
+)
+
+func TestAccSepaInstantAssociation_basic(t *testing.T) {
+	parentOrganisationId := os.Getenv("FORM3_ORGANISATION_ID")
+	organisationId := uuid.NewV4().String()
+	associationId := uuid.NewV4().String()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSepaInstantAssociationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testForm3SepaInstantAssociationConfigA, organisationId, parentOrganisationId, associationId),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSepaInstantAssociationExists("form3_sepainstant_association.association"),
+					resource.TestCheckResourceAttr("form3_sepainstant_association.association", "association_id", associationId),
+					resource.TestCheckResourceAttr("form3_sepainstant_association.association", "organisation_id", organisationId),
+					resource.TestCheckResourceAttr("form3_sepainstant_association.association", "business_user_id", "123456"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSepaInstantAssociationDestroy(state *terraform.State) error {
+	client := testAccProvider.Meta().(*form3.AuthenticatedClient)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "form3_sepainstant_association" {
+			continue
+		}
+
+		response, err := client.AssociationClient.Associations.GetSepainstantID(associations.NewGetSepainstantIDParams().
+			WithID(strfmt.UUID(rs.Primary.ID)))
+
+		if err == nil {
+			return fmt.Errorf("sepa instant record %s still exists, %+v", rs.Primary.ID, response)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckSepaInstantAssociationExists(resourceKey string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceKey]
+
+		if !ok {
+			return fmt.Errorf("not found: %s", resourceKey)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no bacs Record ID is set")
+		}
+
+		client := testAccProvider.Meta().(*form3.AuthenticatedClient)
+
+		foundRecord, err := client.AssociationClient.Associations.GetSepainstantID(associations.NewGetSepainstantIDParams().
+			WithID(strfmt.UUID(rs.Primary.ID)))
+
+		if err != nil {
+			return err
+		}
+
+		if foundRecord.Payload.Data.ID.String() != rs.Primary.ID {
+			return fmt.Errorf("sepa instant record not found expected %s found %s", rs.Primary.ID, foundRecord.Payload.Data.ID.String())
+		}
+
+		return nil
+	}
+}
+
+const testForm3SepaInstantAssociationConfigA = `
+resource "form3_organisation" "organisation" {
+	organisation_id        = "%s"
+	parent_organisation_id = "%s"
+	name 		               = "terraform-organisation"
+}
+
+resource "form3_sepainstant_association" "association" {
+	organisation_id                  = "${form3_organisation.organisation.organisation_id}"
+	association_id                   = "%s"
+	business_user_id                 = "123456"
+}`
