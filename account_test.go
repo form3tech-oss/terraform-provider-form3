1968b4842f4b2ecdf5e6b93396d5ef0ee5d45551
Edward Wilde <ewilde@gmail.com>
2018-07-13 12:26:28 +0100

    Fix code formatting (#20)
    Signed-off-by: Edward Wilde <ewilde@gmail.com>

--- account_test.go
+++ account_test.go
@@ -1,28 +1,28 @@
 package form3
 
 import (
-	"testing"
-	"github.com/ewilde/go-form3/client/accounts"
-	"github.com/ewilde/go-form3/models"
-		"github.com/ewilde/go-form3/client/organisations"
 	"github.com/apex/log"
+	"github.com/ewilde/go-form3/client/accounts"
+	"github.com/ewilde/go-form3/client/organisations"
+	"github.com/ewilde/go-form3/models"
+	"testing"
 )
 
 func TestAccDeleteAccount(t *testing.T) {
 
 	createdBankID, err := auth.AccountClient.Accounts.PostBankids(accounts.NewPostBankidsParams().
 		WithBankIDCreationRequest(&models.BankIDCreation{
-		Data: &models.BankID{
-			Type: "bankids",
-			ID: NewUUIDValue(),
-			OrganisationID: organisationId,
-			Attributes: &models.BankIDAttributes{
-				Country: "GB",
-				BankIDCode: "GBDSC",
-				BankID: "100202",
+			Data: &models.BankID{
+				Type:           "bankids",
+				ID:             NewUUIDValue(),
+				OrganisationID: organisationId,
+				Attributes: &models.BankIDAttributes{
+					Country:    "GB",
+					BankIDCode: "GBDSC",
+					BankID:     "100202",
+				},
 			},
-		},
-	}))
+		}))
 
 	defer func() {
 		if _, err := auth.AccountClient.Accounts.DeleteBankidsID(accounts.NewDeleteBankidsIDParams().WithID(createdBankID.Payload.Data.ID)); err != nil {
@@ -32,18 +32,17 @@ func TestAccDeleteAccount(t *testing.T) {
 		}
 	}()
 
-
 	createdBicID, err := auth.AccountClient.Accounts.PostBics(accounts.NewPostBicsParams().
 		WithBicCreationRequest(&models.BicCreation{
-		Data: &models.Bic{
-			Type: "bankids",
-			ID: NewUUIDValue(),
-			OrganisationID: organisationId,
-			Attributes: &models.BicAttributes{
-				Bic: "NWABCD12",
+			Data: &models.Bic{
+				Type:           "bankids",
+				ID:             NewUUIDValue(),
+				OrganisationID: organisationId,
+				Attributes: &models.BicAttributes{
+					Bic: "NWABCD12",
+				},
 			},
-		},
-	}))
+		}))
 
 	defer func() {
 		if _, err := auth.AccountClient.Accounts.DeleteBicsID(accounts.NewDeleteBicsIDParams().WithID(createdBicID.Payload.Data.ID)); err != nil {
@@ -53,22 +52,21 @@ func TestAccDeleteAccount(t *testing.T) {
 		}
 	}()
 
-
 	createResponse, err := auth.AccountClient.Accounts.PostAccounts(accounts.NewPostAccountsParams().
 		WithAccountCreationRequest(&models.AccountCreation{
-		Data: &models.Account{
-			OrganisationID: UUID(organisationId),
-			Type:           "accounts",
-			ID:             NewUUID(),
-			Attributes: &models.AccountAttributes{
-				AccountNumber: "12345678",
-				BankID: "100202",
-				Bic: "NWABCD12",
-				BankIDCode: "GBDSC",
-				Country: String("GB"),
+			Data: &models.Account{
+				OrganisationID: UUID(organisationId),
+				Type:           "accounts",
+				ID:             NewUUID(),
+				Attributes: &models.AccountAttributes{
+					AccountNumber: "12345678",
+					BankID:        "100202",
+					Bic:           "NWABCD12",
+					BankIDCode:    "GBDSC",
+					Country:       String("GB"),
+				},
 			},
-		},
-	}))
+		}))
 
 	assertNoErrorOccurred(err, t)
 
