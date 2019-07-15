6d8d59aa74e3f814412b55107da32855a66860d8
Mathieu Lornac <mathieu.lornac@form3.tech>
2019-07-15 16:42:39 +0200

    Updating model

--- client/users/get_users_user_id_aces_responses.go
+++ client/users/get_users_user_id_aces_responses.go
@@ -13,7 +13,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/form3tech/terraform-provider-form3/models"
+	models "github.com/form3tech-oss/terraform-provider-form3/models"
 )
 
 // GetUsersUserIDAcesReader is a Reader for the GetUsersUserIDAces structure.
