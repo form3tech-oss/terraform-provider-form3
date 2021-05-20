b0b52c6eebf30f1cb7460975056fcf33bd29bc8a
Juan Pablo Genovese <juan.pablo.genovese@form3.tech>
2021-05-20 15:26:01 +0200

    Fix certificates and expectations

--- api/certificates_test.go
+++ api/certificates_test.go
@@ -1,10 +1,17 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
 	"testing"
+	"time"
 
-	"github.com/form3tech-oss/terraform-provider-form3/api/pkg"
 	"github.com/form3tech-oss/terraform-provider-form3/client/system"
 	"github.com/form3tech-oss/terraform-provider-form3/models"
 	"github.com/go-openapi/swag"
@@ -169,9 +176,9 @@ func deleteCertificate(keysCreated *system.PostKeysCreated, certCreation *system
 
 func createCertificate(createResponse *system.PostKeysCreated, t *testing.T) (*system.PostKeysKeyIDCertificatesCreated, string, string) {
 	id := uuid.New()
-	newCertificate, err := pkg.GenerateSelfSignedCert()
+	newCertificate, err := generateSelfSignedCert()
 	assertNoErrorOccurred(t, err)
-	issuer, err := pkg.GenerateSelfSignedCert()
+	issuer, err := generateSelfSignedCert()
 	assertNoErrorOccurred(t, err)
 	createCertResponse, err := auth.SystemClient.System.PostKeysKeyIDCertificates(system.NewPostKeysKeyIDCertificatesParams().
 		WithKeyID(createResponse.Payload.Data.ID).
@@ -187,4 +194,91 @@ func createCertificate(createResponse *system.PostKeysCreated, t *testing.T) (*s
 		}))
 	assertNoErrorOccurred(t, err)
 	return createCertResponse, newCertificate, issuer
+}
+
+func generateSelfSignedCert() (string, error) {
+	certLength := 2048
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			Organization:  []string{"CA Authority"},
+			Country:       []string{"UK"},
+			Province:      []string{""},
+			Locality:      []string{""},
+			StreetAddress: []string{""},
+			PostalCode:    []string{""},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	caPrivateKey, err := rsa.GenerateKey(rand.Reader, certLength)
+	if err != nil {
+		return "", err
+	}
+
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivateKey.PublicKey, caPrivateKey)
+	if err != nil {
+		return "", err
+	}
+
+	caPEM := new(bytes.Buffer)
+	err = pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	caPrivateKeyPEM := new(bytes.Buffer)
+	err = pem.Encode(caPrivateKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(caPrivateKey),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(1658),
+		Subject: pkix.Name{
+			Organization:  []string{"A Bank"},
+			Country:       []string{"UK"},
+			Province:      []string{""},
+			Locality:      []string{"London"},
+			StreetAddress: []string{"Someaddress"},
+			PostalCode:    []string{"WhoBloodyKnows"},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+
+	certPrivateKey, err := rsa.GenerateKey(rand.Reader, certLength)
+	if err != nil {
+		return "", err
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPrivateKey.PublicKey, caPrivateKey)
+	if err != nil {
+		return "", err
+	}
+
+	certPEM := new(bytes.Buffer)
+	err = pem.Encode(certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return certPEM.String(), nil
 }
